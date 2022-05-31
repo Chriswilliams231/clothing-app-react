055f3ff84b98007e51314dbe8ad6cbf020cd0011
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2022-05-31 14:06:20 -0700

    added a count to the shop icon

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,10 +1,12 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const addCartItem = (cartItems, productToAdd) => {
+  // This finds if cartItem contains the same product as productToAdd
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === productToAdd.id
   );
 
+  //   This condictional is if the product is found, then add the quantity by 1
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
       cartItem.id === productToAdd.id
@@ -13,6 +15,7 @@ export const addCartItem = (cartItems, productToAdd) => {
     );
   }
 
+  //   returns a new array with modified cartItems and or a new array
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
@@ -21,16 +24,33 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (product) => {
     setCartItems(addCartItem(cartItems, product));
   };
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartCount,
+  };
+
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
